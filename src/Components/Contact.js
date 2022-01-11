008ba4c50cbad3d3a02a71e820db3bba149b20a8
Justin Moran <justinmdotnet@outlook.com>
2022-01-10 22:03:40 -0800

    Added Twitter component and changed header-background image

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,17 +1,12 @@
 import React, {Component} from 'react';
+import Twitter from "./Twitter";
 
 class Contact extends Component {
     render() {
-
-        let name;
-        let city;
-        let state;
+        
         let message;
         
         if (this.props.data) {
-            name = this.props.data.name;
-            city = this.props.data.address.city;
-            state = this.props.data.address.state;
             message = this.props.data.contactmessage;
         }
 
@@ -80,40 +75,7 @@ class Contact extends Component {
 
 
                     <aside className="four columns footer-widgets">
-                        <div className="widget widget_contact">
-
-                            <h4>Address and Phone</h4>
-                            <p className="address">
-                                {name}<br/>
-                                {city}, {state}<br/>
-                            </p>
-                        </div>
-
-                        <a className="twitter-timeline" data-theme="dark"
-                           href="https://twitter.com/JustinMDotNet?ref_src=twsrc%5Etfw">Tweets by JustinMDotNet</a>
-                        <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-                        
-                        {/*<div className="widget widget_tweets">*/}
-                        {/*    <h4 className="widget-title">Latest Tweets</h4>*/}
-                        {/*    <ul id="twitter">*/}
-                        {/*        <li>*/}
-                        {/*<span>*/}
-                        {/*This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.*/}
-                        {/*Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum*/}
-                        {/*<a href="#">http://t.co/CGIrdxIlI3</a>*/}
-                        {/*</span>*/}
-                        {/*            <b><a href="#">2 Days Ago</a></b>*/}
-                        {/*        </li>*/}
-                        {/*        <li>*/}
-                        {/*<span>*/}
-                        {/*Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,*/}
-                        {/*eaque ipsa quae ab illo inventore veritatis et quasi*/}
-                        {/*<a href="#">http://t.co/CGIrdxIlI3</a>*/}
-                        {/*</span>*/}
-                        {/*            <b><a href="#">3 Days Ago</a></b>*/}
-                        {/*        </li>*/}
-                        {/*    </ul>*/}
-                        {/*</div>*/}
+                        <Twitter />                        
                     </aside>
                 </div>
             </section>
---
+++ src/Components/Twitter.js
@@ -0,0 +1,28 @@
+﻿import React, { useEffect } from "react";
+
+const Twitter = () => {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://platform.twitter.com/widgets.js";
+        document.getElementsByClassName("twitter-embed")[0].appendChild(script);
+    }, []);
+
+    return (
+        <section className="twitterContainer">
+            <div className="twitter-embed">
+                <div>
+                    Tweets by <a href="https://twitter.com/@JustinMDotNet">@JustinMDotNet</a>
+                </div>
+                <a
+                    className="twitter-timeline"
+                    data-theme="dark"
+                    data-tweet-limit="2"
+                    data-chrome="noheader nofooter noborders"
+                    href="https://twitter.com/JustinMDotNet"
+                />
+            </div>
+        </section>
+    );
+};
+
+export default Twitter;
